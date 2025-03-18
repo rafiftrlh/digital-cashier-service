bada0c5558b9dba5edf90fdc3dfae2774673dea5
rafiftrlh <rafifitro89@gmail.com>
2025-03-18 15:06:08 +0700

    refactor(types): enforce type definitions for function outputs

--- src/product-categories/product-categories.controller.ts
+++ src/product-categories/product-categories.controller.ts
@@ -1,33 +1,33 @@
 import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { ProductCategoriesService } from './product-categories.service';
-import { Prisma } from '@prisma/client';
+import { Prisma, ProductCategory } from '@prisma/client';
 
 @Controller('product-categories')
 export class ProductCategoriesController {
      constructor(private readonly productCategoriesService: ProductCategoriesService) { }
 
      @Get()
-     findAll() {
+     findAll(): Promise<ProductCategory[]> {
           return this.productCategoriesService.findAll();
      }
 
      @Get(':id')
-     findOne(@Param('id') id: string) {
+     findOne(@Param('id') id: string): Promise<ProductCategory | null> {
           return this.productCategoriesService.findOne(+id);
      }
 
      @Post()
-     create(@Body() data: Prisma.ProductCategoryCreateInput) {
+     create(@Body() data: Prisma.ProductCategoryCreateInput): Promise<ProductCategory> {
           return this.productCategoriesService.create(data);
      }
 
      @Put(':id')
-     update(@Param('id') id: string, @Body() data: Prisma.ProductCategoryUpdateInput) {
+     update(@Param('id') id: string, @Body() data: Prisma.ProductCategoryUpdateInput): Promise<ProductCategory> {
           return this.productCategoriesService.update(+id, data);
      }
 
      @Delete(':id')
-     remove(@Param('id') id: string) {
+     remove(@Param('id') id: string): Promise<ProductCategory> {
           return this.productCategoriesService.remove(+id);
      }
 }
--- src/product-categories/product-categories.service.ts
+++ src/product-categories/product-categories.service.ts
@@ -1,35 +1,35 @@
 import { Injectable } from '@nestjs/common';
-import { Prisma } from '@prisma/client';
+import { Prisma, ProductCategory } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class ProductCategoriesService {
      constructor(private prisma: PrismaService) { }
 
-     async findAll() {
+     async findAll(): Promise<ProductCategory[]> {
           return this.prisma.productCategory.findMany();
      }
 
-     async findOne(id: number) {
+     async findOne(id: number): Promise<ProductCategory | null> {
           return this.prisma.productCategory.findUnique({
                where: { id },
           });
      }
 
-     async create(data: Prisma.ProductCategoryCreateInput) {
+     async create(data: Prisma.ProductCategoryCreateInput): Promise<ProductCategory> {
           return this.prisma.productCategory.create({
                data,
           });
      }
 
-     async update(id: number, data: Prisma.ProductCategoryUpdateInput) {
+     async update(id: number, data: Prisma.ProductCategoryUpdateInput): Promise<ProductCategory> {
           return this.prisma.productCategory.update({
                where: { id },
                data,
           });
      }
 
-     async remove(id: number) {
+     async remove(id: number): Promise<ProductCategory> {
           return this.prisma.productCategory.delete({
                where: { id },
           });
